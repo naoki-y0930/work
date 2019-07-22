1a81c73a180705e65e41bdb181d79201b3796771
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-07-22 16:08:10 +0900

    [Finish] 3-5

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
   def new
+  	@post = Post.new
   end
+
+  def create
+  	#ストロングパラメータを使用
+  	post = Post.new(post_params)
+  	#データベースへ保存する
+  	post.save
+  	#トップ画面へリダイレクト
+  	dedirect_to '/top'
+  	
+  end
+
+
+
+private
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end
+
 end
+
--- vagrant/sample_app/config/routes.rb
+++ vagrant/sample_app/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   get 'top' => 'root#top'
 
+  post 'posts' => 'posts#create'
+
 end
