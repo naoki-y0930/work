a5b802f7209f7cd3b4205fd4c1bc30e711610d6c
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-07-23 10:22:59 +0900

    [Start] 3-7

--- vagrant/sample_app/config/routes.rb
+++ vagrant/sample_app/config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'posts' => 'posts#index'
   get 'posts/:id' => 'posts#show', as:'post'
   #.../posts/1や.../posts/3に該当する
+  get 'posts/:id/edit' => 'posts#edit', as:'edit_post'
 end
