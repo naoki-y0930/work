5e8dec9fd091edabbdcd7aba90475d108c4be88f
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-07-23 11:07:23 +0900

    [Finish] 3-7

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -21,6 +21,15 @@ class PostsController < ApplicationController
   end
 
 
+  def edit
+	@post = Post.find(params[:id])
+  end
+
+  def update
+	post = Post.find(params[:id])
+	post.update(post_params)
+	redirect_to post_path(post.id)
+  end
 
 private
   def post_params
